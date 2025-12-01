0294f001f35f2f63a6fb3e908d71d07e97081f42
Edgar <edgar.muoni@gmail.com>
2025-12-01 16:21:05 +0200

    As an app developer I want to get a list of all <KokteilideretseptuuriNimekiri> to show in my app.

--- controllers/CocktailRecipesController.js
+++ controllers/CocktailRecipesController.js
@@ -10,3 +10,27 @@ async (req, res) => {
     .status(200)
     .send(recipes.map(({RecipeID, Name}) => {return{RecipeID, Name}}))
 }
+
+exports.getByID =
+async (req, res) => {
+    const film = await getRecipe(req, res);
+    if (!Recipe) {return res.status(404).send("Recipe not found")};
+    res
+    .status(200)
+    .send(Recipe);
+}
+
+const getRecipe =
+async (req, res) => {
+    const idNumber = req.params.RecipeID;
+    if(isNaN(idNumber)) {
+        res.status(400).send({error: `Entered ID is not valid ${idNumber}`});
+        return null;
+    }
+    const film = await db.recipes.findByPk(idNumber);
+    if(!film) {
+        res.status(400).send({Error: `Entered ID is not valid ${idNumber}`});
+        return null;
+    }
+    return recipe;
+}
--- controllers/Utilities.js
+++ controllers/Utilities.js
@@ -1,12 +1,5 @@
 const {db} = require('../db')
-const Utilities = require('./Utilities')
-
-exports.getAll =
-async (req,res) => {
-    const films = await db.films.findAll();
-    console.log("getAll: "+ recipes )
-    res
-    .status(200)
-    .send(films.map(({RecipeID, Name}) => {return{RecipeID, Name}}))
+exports.getBaseURL = (req) => {
+    return (req.connection && req.connection.encrypted ? "https":"http")+`://${req.headers.host}`;
 }
 
