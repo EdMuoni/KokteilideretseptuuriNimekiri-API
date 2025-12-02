7f94bf9b6194fd96458662b0c83aeb48b775e935
Edgar <edgar.muoni@gmail.com>
2025-12-02 14:36:48 +0200

    #435875 As an app developer I want to be able to add a new <Recipe>
    Code backend, and yaml documentation for the specified card/user story.

--- controllers/CocktailRecipesController.js
+++ controllers/CocktailRecipesController.js
@@ -11,3 +11,48 @@ async (req, res) => {
     .send(recipes.map(({RecipeID, Name}) => {return{RecipeID, Name}}))
 }
 
+exports.getByID =
+async (req, res) => {
+    const recipe = await getRecipe(req, res);
+    if (!recipe) {return res.status(404).send("Recipe not found")};
+    res
+    .status(200)
+    .send(recipe);
+}
+
+exports.create = 
+async (req, res) => {
+    if(
+        !req.body.Name ||
+        !req.body.Description ||
+        !req.body.Beverage ||
+        !req.body.UserScore
+    ) {
+        return res.status(400).send({error: "Missing some parameter, please review your request data."});
+    }
+        const newRecipe = {
+        Name: req.body.Name,
+        Description: req.body.Description,
+        Beverage: req.body.Beverage,
+        UserScore: req.body.UserScore
+    }
+        const createdRecipe = await db.recipes.create(newRecipe);
+        return res
+        .location(`${Utilities.getBaseURL(req)}/recipes/${createdRecipe.RecipeID}`)
+        .sendStatus(201);  
+}
+
+const getRecipe =
+async (req, res) => {
+    const idNumber = req.params.RecipeID;
+    // if(isNaN(idNumber)) {
+    //     res.status(400).send({error: `Entered ID is not valid ${idNumber}`});
+    //     return null;
+    // }
+    const recipe = await db.recipes.findByPk(idNumber);
+    if(!recipe) {
+        res.status(400).send({Error: `Entered ID is not valid ${idNumber}`});
+        return null;
+    }
+    return recipe;
+}
