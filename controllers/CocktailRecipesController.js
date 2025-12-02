a7578b1c0cb2c67cc1ad0052a9b9d59981f2a68c
Edgar <edgar.muoni@gmail.com>
2025-12-02 12:41:00 +0200

    Update

---
+++ controllers/CocktailRecipesController.js
@@ -0,0 +1,15 @@
+const { UUID } = require('sequelize');
+const {db} = require('../db');
+const Utilities = require('./Utilities.js');
+const UUID = require('uuid');
+
+exports.getAll = 
+async (req, res) => {
+    const recipes = await db.recipes.findAll();
+    console.log("getAll: " + recipes)
+    res
+    .status(200)
+    .send(recipes.map(({RecipeID, Name}) => {return{RecipeID, 
+        Name}}))
+}
+
