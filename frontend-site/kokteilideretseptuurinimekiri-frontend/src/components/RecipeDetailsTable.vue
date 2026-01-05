889afddbfa6c93173cff0179213c36bef7bf32e1
Edgar <edgar.muoni@gmail.com>
2026-01-05 16:04:34 +0200

    ReInstalled vue and had to rewrite front-end code

--- frontend-site/kokteilideretseptuurinimekiri-frontend/src/components/RecipeDetailsTable.vue
+++ frontend-site/kokteilideretseptuurinimekiri-frontend/src/components/RecipeDetailsTable.vue
@@ -1,14 +1,47 @@
 <script>
+import router from "../router";
+
 export default {
-  name: "RecipeDetailsTable",
+  name: "RecipesTable",
   props: {
-    item: {
-      RecipeID: "",
-      Name: "",
-      Description: "desc",
-      Beverage: "",
-      UserScore: 2.5,
+    items: Array,
+  },
+  methods: {
+    async deleteRecipe(RecipeID) {
+      await await fetch(`http://localhost:8080/recipe/${this.RecipeID}`, {
+        method: "DELETE",
+      });
     },
   },
 };
 </script>
+
+<template>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Recipe ID</th>
+        <th>Name</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="item in items" :key="item.RecipeID">
+        <td>{{ item.RecipeID }}</td>
+        <td>{{ item.Name }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'recipe', params: { seekID: item.RecipeID } }"
+          >
+            <button>View Details</button>
+          </router-link>
+        </td>
+        <td>
+          <router-link>
+            <button @click="deleteRecipe(item.RecipeID)">Delete</button>
+          </router-link>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
